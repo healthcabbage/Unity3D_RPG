4b754c7d84f70cbfda4b5e0f55452942ca790777
Cabbage <20172367@g.dongseo.ac.kr>
2020-07-17 03:27:48 +0900

    1일차 작업
    - 스타트 화면 버튼 작업
    - 화면 페이드 인 아웃 (작업중)
    - 사용할 3D캐릭터 넣어둠

---
+++ 3D_RPG/Assets/Script/FadeIn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeIn : MonoBehaviour
+{
+    public float FadeTime = 2f;
+    //Image fadeImg;
+    float start;
+    float end;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 3D_RPG/Assets/Script/StartMenu_Button.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartMenu_Button : MonoBehaviour
+{
+    public void StartButton()
+    {
+        SceneManager.LoadScene("SelectScene");
+    }
+
+    public void ExitButton()
+    {
+        
+    }
+}
