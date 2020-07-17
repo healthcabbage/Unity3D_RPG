35ed6d8297450b397a048fe5620006c2b49f6b89
건강 보조 식품 양배추 <49223403+healthcabbage@users.noreply.github.com>
2020-07-17 18:16:48 +0900

    2일차_1
    - Fade In,Out 작업
    - 게임 종료 버튼
    - 캐릭터 선택창 (작업중)
    - 옵션창 (작업중)

--- 3D_RPG/Assets/Script/FadeIn.cs
+++ 3D_RPG/Assets/Script/FadeIn.cs
@@ -1,23 +1,92 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FadeIn : MonoBehaviour
 {
-    public float FadeTime = 2f;
-    //Image fadeImg;
+    public float FadeTime;
+    public Image fadeImg;
     float start;
     float end;
-    
-    // Start is called before the first frame update
-    void Start()
+    float time = 0f;
+    bool isPlaying = false;
+    public bool fadeselect;
+
+    void Awake()
     {
-        
+        fadeImg = GetComponent<Image>();
+        if (fadeselect != false)
+        {
+            InStartFadeAnim();
+        }
+        else
+        {
+            OutStartFadeAnim();
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void InStartFadeAnim()
     {
-        
+        if (isPlaying == true) //중복방지
+        {
+            return;
+        }
+
+        start = 1f;
+        end = 0;
+
+        StartCoroutine("fadeInplay");
+    }
+
+    IEnumerator fadeInplay()
+    {
+        isPlaying = true;
+
+        Color fadecolor = fadeImg.color;
+        time = 0f;
+        fadecolor.a = Mathf.Lerp(start, end, time);
+
+        while (fadecolor.a > 0f)
+        {
+            time += Time.deltaTime / FadeTime;
+            fadecolor.a = Mathf.Lerp(start, end, time);
+            fadeImg.color = fadecolor;
+            yield return null;
+        }
+
+        isPlaying = false;
+    }
+
+    public void OutStartFadeAnim()
+    {
+        if (isPlaying == true)
+        {
+            return;
+        }
+
+        start = 0f;
+        end = 1f;
+        StartCoroutine("fadeoutplay");
+    }
+
+    IEnumerator fadeoutplay()
+    {
+        isPlaying = true;
+
+        Color fadecolor = fadeImg.color;
+        time = 0f;
+        fadecolor.a = Mathf.Lerp(start, end, time);
+
+        while (fadecolor.a < 255f)
+        {
+            time += Time.deltaTime / FadeTime;
+            fadecolor.a = Mathf.Lerp(start, end, time);
+            fadeImg.color = fadecolor;
+            yield return null;
+        }
+
+        isPlaying = false;
     }
 }
---
+++ 3D_RPG/Assets/Script/MenuSystem.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuSystem : MonoBehaviour
+{
+    public Text Resolution;
+    public Button Start;
+    public Button Option;
+    public Button Exit;
+    public Button Return;
+    public Text Volume;
+    public Image Fade;
+
+    bool isoption = true;
+
+    public void StartButton()
+    {
+        Fade.gameObject.SetActive(true);
+        Invoke("InVokeMove", 2f);
+    }
+
+    void InVokeMove()
+    {
+        SceneManager.LoadScene("SelectScene");
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
+    public void OptionButton()
+    {
+        if (isoption != true)
+        {
+            OffOption();
+            isoption = true;
+        }
+        else
+        {
+            OnOption();
+            isoption = false;
+        }
+    }
+
+    void OnOption()
+    {
+        Resolution.gameObject.SetActive(true);
+        Start.gameObject.SetActive(false);
+        Option.gameObject.SetActive(false);
+        Exit.gameObject.SetActive(false);
+        Return.gameObject.SetActive(true);
+        Volume.gameObject.SetActive(true);
+    }
+
+    void OffOption()
+    {
+        Resolution.gameObject.SetActive(false);
+        Start.gameObject.SetActive(true);
+        Option.gameObject.SetActive(true);
+        Exit.gameObject.SetActive(true);
+        Return.gameObject.SetActive(false);
+        Volume.gameObject.SetActive(false);
+    }
+}
