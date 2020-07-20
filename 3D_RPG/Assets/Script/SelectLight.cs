a61cae4f9f1883c2efff198cc5ee850952930c48
건강 보조 식품 양배추 <49223403+healthcabbage@users.noreply.github.com>
2020-07-20 17:33:10 +0900

    3일차
    - 캐릭터 선택창

---
+++ 3D_RPG/Assets/Script/SelectLight.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectLight : MonoBehaviour
+{
+    public GameObject playerOne;
+    public GameObject playerTwo;
+    bool Onecheck = false;
+    bool Twocheck = false;
+    public GameObject night;
+    public GameObject wizard;
+    public static int characterNum = 0;
+
+    public void SelectOneCharacter()
+    {
+        if (Onecheck != true)
+        {
+            playerOne.SetActive(true);
+            night.SetActive(true);
+            Onecheck = true;
+            if (Twocheck == true)
+            {
+                playerTwo.SetActive(false);
+                wizard.SetActive(false);
+                Twocheck = false;
+            }
+        }
+        else
+        {
+            playerOne.SetActive(false);
+            night.SetActive(false);
+            Onecheck = false;
+        }
+    }
+
+    public void SelectTwoCharacter()
+    {
+        if (Twocheck != true)
+        {
+            playerTwo.SetActive(true);
+            wizard.SetActive(true);
+            Twocheck = true;
+            if (Onecheck == true)
+            {
+                playerOne.SetActive(false);
+                night.SetActive(false);
+                Onecheck = false;
+            }
+        }
+        else
+        {
+            playerTwo.SetActive(false);
+            wizard.SetActive(false);
+            Twocheck = false;
+        }
+    }
+
+    public void NextStage()
+    {
+        
+    }
+}
