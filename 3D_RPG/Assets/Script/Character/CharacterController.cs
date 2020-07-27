fdce1c14560435c6627f08e183cbf166086e26f6
건강 보조 식품 양배추 <49223403+healthcabbage@users.noreply.github.com>
2020-07-27 18:29:36 +0900

    캐릭터 작업
    - 움직일때 달리는 동작 실행
    - 마우스 클릭 시 일반 공격

--- 3D_RPG/Assets/Script/Character/CharacterController.cs
+++ 3D_RPG/Assets/Script/Character/CharacterController.cs
@@ -8,16 +8,17 @@ public class CharacterController : MonoBehaviour
     float hAxis;
     float vAxis;
     Vector3 movement;
+    Animator anim;
 
     void Start()
     {
-
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
         Run();
-        Turn();
+        Attack();
     }
 
     void Run()
@@ -28,10 +29,24 @@ public class CharacterController : MonoBehaviour
         movement = new Vector3(hAxis, 0, vAxis).normalized;
 
         transform.position += movement * speed * Time.deltaTime;
+
+        transform.LookAt(transform.position + movement);
+
+        anim.SetBool("isRun", movement != Vector3.zero);
     }
 
-    void Turn()
+    void Attack()
     {
-        transform.LookAt(transform.position + movement);
+        if(Input.GetMouseButton(0))
+        {
+            Debug.Log("확인");
+            anim.SetBool("isAttack", true);
+            Invoke("Idle", 1f);
+        }
+    }
+
+    void Idle()
+    {
+        anim.SetBool("isAttack", false);
     }
 }
