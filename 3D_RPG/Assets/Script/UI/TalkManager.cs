c80519264e2659db7ac348d90eced8231d168d01
Cabbage <20172367@g.dongseo.ac.kr>
2021-05-07 02:42:04 +0900

    몬스터 보석, 스크립트 수정
    몬스터에게 드랍 될 보석 추가, 대화 스크립트 수정 중, 보석 UI 추가

--- 3D_RPG/Assets/Script/UI/TalkManager.cs
+++ 3D_RPG/Assets/Script/UI/TalkManager.cs
@@ -17,10 +17,10 @@ public class TalkManager : MonoBehaviour
     void GenerateDate()
     {
         nameData.Add(1, new string[] {"낡은 표지판", "낡은 표지판"});
-        talkData.Add(1, new string[] {"북쪽으로 가면 마을이, 서쪽으로 가면 버섯숲이 나온다.", "현재 동쪽은 출입금지이다. 동쪽에는 ----가 있으니 --해야된다"});
+        talkData.Add(1, new string[] {"북쪽으로 가면 마을이, 서쪽으로 가면 버섯숲이 나온다.", "현재 동쪽은 출입금지이다.\n동쪽에는 ----가 있으니 --해야된다"});
 
-        nameData.Add(100, new string[] {"마을소녀", "마을소녀", "마을소녀"});
-        talkData.Add(100, new string[] {"안녕하세요.", "처음 보시는 분이네요.", "마을에는 무슨 일이신가요?"});
+        nameData.Add(100, new string[] {"마을소녀", "마을소녀"});
+        talkData.Add(100, new string[] {"안녕하세요.\n처음 보시는 분이네요.", "마을에는 무슨 일이신가요?"});
 
         talkData.Add(200, new string[] {"아무런 소리가 들리지 않는다", "...자세히 들어보니 작은 인기척이 느껴지는 것 같다."});
 
