330e82c8940b19e1cf767b4d1353a24efbe1801d
Cabbage <20172367@g.dongseo.ac.kr>
2021-05-24 16:59:35 +0900

    아이템 드랍
    - 버섯, 슬라임 몬스터한테서 보석이 2개만 드랍되게 설정
    - 파란 보석, 주황 보석에 스크립트를 만들어 적용

---
+++ 3D_RPG/Assets/Script/Etc/Gem_R.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem_R : MonoBehaviour
+{
+    public bool isAnimated = false;
+    public bool isRotating = false;
+
+    public bool bluerotation = false;
+
+    public Vector3 rotationAngle;
+    public float rotationSpeed;
+
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(bluerotation)
+        {
+            transform.eulerAngles = new Vector3(-90, 0, 0);
+            transform.position = new Vector3(transform.position.x, 1, transform.position.z);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isAnimated)
+        {
+            if(isRotating)
+            {
+                transform.Rotate(rotationAngle * rotationSpeed * Time.deltaTime);
+            }
+        }
+    }
+}
